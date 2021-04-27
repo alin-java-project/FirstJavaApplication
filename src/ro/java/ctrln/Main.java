f3cf813ce467650ffbb11177ac1510fcfa10186d
Alin <alin.ardelean70@gmail.com>
2021-04-27 17:47:27 +0300

    Adaugare noi comentarii pe o singura linie

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -3,6 +3,7 @@ package ro.java.ctrln;
 public class Main {
         // HOME JAVA
         // inca nu inteleg bine sintaxa cod operatiunile de push si download structura locatii
+    // Aceasta este metoda main care porneste o aplicatie Java
     public static void main(String[] args) {
         System.out.println("Invat limbajul Java!");
     }
