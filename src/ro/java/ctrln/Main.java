6e495fd504987e8f5924bc94a8c3af4d79ad938f
Alin <alin.ardelean70@gmail.com>
2021-04-27 13:14:38 +0300

    Mesaj Home Java dorinta de intelegere

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -2,6 +2,7 @@ package ro.java.ctrln;
 
 public class Main {
         // HOME JAVA
+        // inca nu inteleg bine sintaxa cod operatiunile de push si download structura locatii
     public static void main(String[] args) {
         System.out.println("Invat limbajul Java!");
     }
